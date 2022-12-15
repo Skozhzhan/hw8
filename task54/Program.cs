60a9c06aa8a52831a8936274b07fc7043b403bd7
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-12-15 20:58:46 +0300

    упорядочивает по убыванию элементы массива

---
+++ task54/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] NewRndArr(int rows, int col, int min, int max)
+{
+    int[,] arr = new int[rows, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j < arr.GetLength(1) - 1)
+                Console.Write($"{arr[i, j],4}, ");
+            else Console.Write($"{arr[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void Sort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] arr = NewRndArr(4, 4, 1, 9);
+Console.WriteLine("Изначальный массив:");
+PrintArr(arr);
+Console.WriteLine();
+Sort(arr);
+Console.WriteLine("Массив после изменения:");
+PrintArr(arr);
